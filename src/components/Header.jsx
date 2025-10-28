35530a1ec7ca5bcd146a64300a18a341a7ee685d
Shmuye <shmuye27@gmail.com>
2025-10-28 16:16:49 +0300

    initialized amazon clone project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/Header.css"
+const Header = () => {
+    return (
+        <div>
+            <img
+                className="header_logo"
+                src="https://i.ibb.co/000000/logo.png"
+                alt="logo"
+            />
+        </div>
+    )
+}
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAByIx3KKVPJofurVc1FIbx8e24CzDVOz8",
+    authDomain: "clone-938d8.firebaseapp.com",
+    projectId: "clone-938d8",
+    storageBucket: "clone-938d8.firebasestorage.app",
+    messagingSenderId: "611481570389",
+    appId: "1:611481570389:web:eddb4f9fd6893224ced8dc",
+    measurementId: "G-K62QDSVRWL"
+};
