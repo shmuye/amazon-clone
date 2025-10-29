78e99d5e6f6557115176b5288319d471280a1388
Shmuye <shmuye27@gmail.com>
2025-10-29 16:47:41 +0300

    style the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Header from './components/Header';
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Header />
+        </div>
     )
 }
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,44 @@ import React from 'react'
 import "../css/Header.css"
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <img
                 className="header_logo"
-                src="https://i.ibb.co/000000/logo.png"
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
                 alt="logo"
             />
+            <div className="header_search">
+                <input
+                    className="header_searchInput"
+                    type="text"
+                />
+            </div>
+            <div className="header_nav">
+                 <div className="header_option">
+                       <span className="header_optionLineOne">
+                           Hello Guest
+                       </span>
+                       <span className="header_optionLineTwo">
+                           Sign In
+                       </span>
+                 </div>
+                 <div className="header_option">
+                       <span className="header_optionLineOne">
+                           Returns
+                       </span>
+                     <span className="header_optionLineTwo">
+                           Orders
+                       </span>
+                 </div>
+                <div className="header_option">
+                      <span className="header_optionLineOne">
+                            Your
+                       </span>
+                    <span className="header_optionLineTwo">
+                           Prime
+                       </span>
+                 </div>
+            </div>
         </div>
     )
 }
